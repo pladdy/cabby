ac7d106c45221bc2ac0f4c06ad054a8bc63f5418
Matt Pladna <pladdypants@gmail.com>
2018-06-03 14:44:55 -0400

    add to test

--- backend_sqlite_test.go
+++ backend_sqlite_test.go
@@ -125,6 +125,7 @@ func TestSQLiteReadScanError(t *testing.T) {
 		{s.readDiscovery},
 		{s.readManifest},
 		{s.readRoutableCollections},
+		{s.readStatus},
 		{s.readStixObject},
 		{s.readStixObjects},
 		{s.readUser},
