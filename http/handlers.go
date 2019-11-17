d51efbfceff0e2b011d4fe7ddfbd30ef17819c7e
Matt Pladna <pladdypants@gmail.com>
2019-11-17 17:44:36 -0500

    Split up checks; fix version

--- http/handlers.go
+++ http/handlers.go
@@ -11,15 +11,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func handleUndefinedRoute(w http.ResponseWriter, r *http.Request) {
-	resourceNotFound(w, fmt.Errorf("Invalid path: %v", r.URL))
-}
-
 // RequestHandler interface for handling requests
 type RequestHandler interface {
 	Delete(w http.ResponseWriter, r *http.Request)
 	Get(w http.ResponseWriter, r *http.Request)
 	Post(w http.ResponseWriter, r *http.Request)
+}
+
+func handleUndefinedRoute(w http.ResponseWriter, r *http.Request) {
+	resourceNotFound(w, fmt.Errorf("Invalid path: %v", r.URL))
 }
 
 func verifyRequestHeader(r *http.Request, h, v string) bool {
@@ -43,8 +43,13 @@ func WithAcceptSet(h http.HandlerFunc, accept string) http.HandlerFunc {
 
 func withBasicAuth(h http.Handler, us cabby.UserService) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		u, p, ok := r.BasicAuth()
 		r = withTransactionID(r)
+
+		u, p, ok := r.BasicAuth()
+		if !ok {
+			internalServerError(w, errors.New("Authentication failed, try again"))
+			return
+		}
 
 		user, err := us.User(r.Context(), u, p)
 		if err != nil {
@@ -52,9 +57,9 @@ func withBasicAuth(h http.Handler, us cabby.UserService) http.Handler {
 			return
 		}
 
-		if !ok || !user.Defined() {
-			log.WithFields(log.Fields{"user": u}).Warn("User authentication failed!	")
-			unauthorized(w, errors.New("Invalid user/pass combination"))
+		if !user.Defined() {
+			log.WithFields(log.Fields{"user": u}).Warn("User authentication failed!")
+			unauthorized(w, errors.New("Invalid user / password combination"))
 			return
 		}
 
@@ -68,6 +73,11 @@ func withBasicAuth(h http.Handler, us cabby.UserService) http.Handler {
 		log.WithFields(log.Fields{"user": u}).Info("User authenticated")
 		h.ServeHTTP(withHSTS(w), r.WithContext(cabby.WithUser(r.Context(), user)))
 	})
+}
+
+func withHSTS(w http.ResponseWriter) http.ResponseWriter {
+	w.Header().Add("Strict-Transport-Security", "max-age="+sixMonthsOfSeconds+"; includeSubDomains")
+	return w
 }
 
 func withLogging(h http.Handler) http.Handler {
